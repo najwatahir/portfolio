181611871b1bffa7a7f04bae1acf4eedea28227f
Najwa Tahir <110643223+najwatahir@users.noreply.github.com>
2023-03-11 13:39:41 +0800

    Update Project.tsx

--- components/Project.tsx
+++ components/Project.tsx
@@ -16,50 +16,50 @@ const project = [
 
 const Project = () => {
     return (
-        <section id="project" className="bg-[#F1F1F1] pt-5 pb-20">
-            <h1 className="my-10 text-center font-bold text-2xl md:text-4xl text-[#1E1E1E]">
+        <section id="project" className="bg-[#F1F1F1] pb-20 pt-5">
+            <h1 className="my-5 text-center font-bold text-2xl md:text-4xl text-[#1E1E1E]">
                 PROJECT
                 <hr className="w-12 h-1 mx-auto my-4 bg-[#B19DC5] border-0 rounded"></hr>
             </h1>
             <div className="container px-5 py-2 mx-auto">
                 <div className="flex flex-wrap -m-4 justify-center">
                     <div className="p-4 sm:w-1/2 lg:w-1/4">
-                        <div className="h-fit border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+                        <div className="h-fit border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden cursor-default">
                             <img className="lg:h-40 w-full object-cover object-center"
                                 src="/images/todo.jpg" alt="blog" />
-                            <div className="p-6 hover:bg-[#B19DC5] bg-[#1E1E1E] text-[#D9D9D9] hover:text-[#1E1E1E] transition duration-300 ease-in">
+                            <div className="p-6 bg-[#1E1E1E] text-[#D9D9D9]">
                                 <h1 className="text-2xl font-semibold mb-3">A Simple To-Do List</h1>
                                 <p className="leading-relaxed mb-3">My first small project I made with HTML, CSS, and JavaScript. The design is very simple because I made this just for fun & fill my boredness time.</p>
                                 <div className="flex items-center flex-wrap ">
-                                    <a className="text-[#1E1E1E] hover:text-[#8396D6] text-semibold inline-flex items-center md:mb-2 lg:mb-0" href="https://purple-to-do.netlify.app/" target="_blank"><button className="font-semibold px-8 py-3 rounded shadow text-[#1E1E1E] hover:text-[#0F1C4B] bg-[#FFFBEB] hover:bg-[#EDEDED]">Visit</button>
+                                    <a href="https://purple-to-do.netlify.app/" target="_blank"><button className="send-button">Visit</button>
                                     </a>
                                 </div>
                             </div>
                         </div>
                     </div>
                     <div className="p-4 sm:w-1/2 lg:w-1/4 lg:m-0">
-                        <div className="h-fit border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+                        <div className="h-fit border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden curson-default">
                             <img className="lg:h-40 md:h-48 w-full object-cover object-center"
                                 src="/images/wordpressProject.jpg" alt="blog" />
-                            <div className="p-6 hover:bg-[#B19DC5] bg-[#1E1E1E] text-[#D9D9D9] hover:text-[#1E1E1E] transition duration-300 ease-in">
+                            <div className="p-6 bg-[#1E1E1E] text-[#D9D9D9]">
                                 <h1 className="text-2xl font-semibold mb-3">Personal WordPress Site</h1>
                                 <p className="leading-relaxed mb-3">A personal WordPress site I made for my school OOP class project. The contents are flat because I didnt have idea what I want to put.</p>
                                 <div className="flex items-center flex-wrap ">
-                                    <a className="text-[#1E1E1E] hover:text-[#8396D6] text-semibold inline-flex items-center md:mb-2 lg:mb-0" href="https://wawaffle.wordpress.com/" target="_blank"><button className="font-semibold px-8 py-3 hover:bg-[#1E1E1E] rounded shadow text-[#1E1E1E] hover:text-[#0F1C4B] bg-[#FFFBEB] hover:bg-[#EDEDED]">Visit</button>
+                                    <a href="https://wawaffle.wordpress.com/" target="_blank"><button className="send-button">Visit</button>
                                     </a>
                                 </div>
                             </div>
                         </div>
                     </div>
                     <div className="p-4 sm:w-1/2 lg:w-1/4">
-                        <div className="h-fit border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+                        <div className="h-fit border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden cursor-default">
                             <img className="lg:h-40 md:h-48 w-full object-cover object-center"
                                 src="/images/pulaubali.jpg" alt="blog" />
-                            <div className="p-6 hover:bg-[#B19DC5] bg-[#1E1E1E] text-[#D9D9D9] hover:text-[#1E1E1E] transition duration-300 ease-in">
+                            <div className="p-6 bg-[#1E1E1E] text-[#D9D9D9]">
                                 <h1 className="text-2xl font-semibold mb-3">Pulau Bali Website</h1>
                                 <p className="leading-relaxed mb-3">A school project that I made with my friends in group. This project we made on Web Programming class. We use HTML, CSS, and little bit of JavaScript.</p>
                                 <div className="flex items-center flex-wrap ">
-                                    <a className="text-semibold inline-flex items-center md:mb-2 lg:mb-0" href="https://madekarma.github.io/SoulSilver/Bali/project1.html" target="_blank"><button className="font-semibold px-8 py-3 rounded shadow text-[#1E1E1E] hover:text-[#0F1C4B] bg-[#FFFBEB] hover:bg-[#EDEDED]">Visit</button>
+                                    <a href="https://madekarma.github.io/SoulSilver/Bali/project1.html" target="_blank"><button className="send-button">Visit</button>
                                     </a>
                                 </div>
                             </div>

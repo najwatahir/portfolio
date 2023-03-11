3324e31495785f32ef7ce9d96354dbf77cc70a54
Najwa Tahir <110643223+najwatahir@users.noreply.github.com>
2023-03-11 16:20:10 +0800

    Update Project.tsx

--- components/Project.tsx
+++ components/Project.tsx
@@ -4,16 +4,6 @@ import Link from "next/link"
 import SlideUp from "./Slideup"
 import { BsGithub, BsArrowUpRightSquare } from "react-icons/bs"
 
-const project = [
-    {
-        image: "/images/umaiApp.jpg",
-    },
-    {
-        image: "/images/wordpressProject.jpg",
-        link: "https://wawaffle.wordpress.com/",
-    },
-]
-
 const Project = () => {
     return (
         <section id="project" className="bg-[#F1F1F1] pb-20 pt-5">
@@ -26,7 +16,7 @@ const Project = () => {
                     <div className="project-box sm:w-1/2 lg:w-1/4 rounded-lg h-1/4">
                         <div className="h-fit rounded-lg overflow-hidden cursor-default">
                             <img className="lg:h-40 w-screen object-cover object-center"
-                                src="/images/todo.jpg" alt="blog" />
+                                src="/images/todo.jpg" alt="Simple To-Do List" />
                             <div className="p-4">
                                 <h1 className="text-xl font-semibold mb-3">A Simple To-Do List</h1>
                                 <p className="leading-relaxed mb-3">My first small project I made with HTML, CSS, and JavaScript. The design is very simple because I made this just for fun & fill my boredness time.</p>
@@ -40,7 +30,7 @@ const Project = () => {
                     <div className="project-box sm:w-1/2 lg:w-1/4 rounded-lg h-1/4">
                         <div className="h-fit rounded-lg overflow-hidden cursor-default">
                             <img className="lg:h-40 md:h-48 w-screen object-cover object-center"
-                                src="/images/wordpressProject.jpg" alt="blog" />
+                                src="/images/wordpressProject.jpg" alt="Personal Website with WordPress" />
                             <div className="p-4">
                                 <h1 className="text-2xl font-semibold mb-3">Personal WordPress Site</h1>
                                 <p className="leading-relaxed mb-3">A personal website I created with WordPress for my school OOP class project. The contents are flat because I didnt have idea what I want to put.</p>
@@ -54,7 +44,7 @@ const Project = () => {
                     <div className="project-box sm:w-1/2 lg:w-1/4 rounded-lg h-1/4">
                         <div className="h-fit rounded-lg overflow-hidden cursor-default">
                             <img className="lg:h-40 w-screen object-cover object-center"
-                                src="/images/projectbali.jpg" alt="blog" />
+                                src="/images/projectbali.jpg" alt="Website about Pulau Bali" />
                             <div className="p-4">
                                 <h1 className="text-2xl font-semibold mb-3">Pulau Bali Website</h1>
                                 <p className="leading-relaxed mb-3">A school project that I made with my friends in group. This project we made on Web Programming class. We use HTML, CSS, and little bit of JavaScript.</p>

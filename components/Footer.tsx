f79c2dbbee1c005206cca87ff94a5bd87da0686c
Najwa Tahir <110643223+najwatahir@users.noreply.github.com>
2023-06-01 14:47:19 +0800

    Add files via upload

---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <section id="project" className="bg-[#DAD7E3]">
+            <p className="text-xl text-semibold text-[#1E1E1E] text-center pt-5">Created by Najwa ( ˶ˆ꒳ˆ˵ )</p>
+
+            <p className="text-semibold text-[#1E1E1E] text-center p-5">Copyright &#169; 2023 Najwa Tahir. All right reserved.</p>
+        </section>
+    )
+}

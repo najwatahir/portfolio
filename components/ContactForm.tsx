a917c6294b0175895a5820d1faba12b58c5b9fb9
Najwa Tahir <110643223+najwatahir@users.noreply.github.com>
2023-06-01 14:45:18 +0800

    change background color

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -7,7 +7,7 @@ function ContactForm() {
 
     const [state, handleSubmit] = useForm("mjvdovve");
     if (state.succeeded) {
-        return <html>
+        return <main>
             <body className="bg-[#202635]">
                 <div className="container px-5 py-2 mx-auto">
                     <div className="flex flex-wrap -m-4 justify-center">
@@ -21,18 +21,18 @@ function ContactForm() {
                     </div>
                 </div>
             </body>
-        </html>
+        </main>
     }
 
     return (
-        <section id="contact" className="bg-[#202635] body-font relative">
+        <section id="contact" className="bg-[#EDEDED] body-font relative">
             <div className="container px-5 py-10 mx-auto">
                 <div className="flex flex-col text-center w-full mb-12">
-                    <h1 className="my-2 text-center font-bold text-2xl md:text-4xl text-[#D9D9D9]">
+                    <h1 className="my-2 text-center font-bold text-2xl md:text-4xl text-[#1E1E1E]">
                         CONTACT
                         <hr className="w-12 h-1 mx-auto my-4 bg-[#B19DC5] border-0 rounded"></hr>
                     </h1>
-                    <p className="lg:w-2/3 mx-auto leading-relaxed text-base md:text-xl text-[#D9D9D9]">Write me your message, question, or anything here!</p>
+                    <p className="lg:w-2/3 mx-auto leading-relaxed text-base md:text-xl text-[#1E1E1E]">Write me your message, question, or anything here!</p>
                 </div>
                 <div className="lg:w-1/2 md:w-2/3 mx-auto">
                     <div className="flex flex-wrap -m-2 justify-center">
@@ -40,10 +40,10 @@ function ContactForm() {
                             <div className="relative">
                                 <form action="https://formspree.io/f/mjvdovve"
                                     method="POST" onSubmit={handleSubmit} autoComplete="off">
-                                    <label className="leading-7 text-sm text-[#D9D9D9]" htmlFor="name">
+                                    <label className="leading-7 text-sm text-[#1E1E1E]" htmlFor="name">
                                         Name
                                     </label>
-                                    <input className="w-full bg-[#1D202B] bg-opacity-40 rounded border border-[#4E597A] focus:border-[#20232E] focus:bg-[#15171E] focus:ring-2 focus:ring-[#B19DC5] text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                                    <input className="w-full bg-[#1D202B]  rounded border border-[#4E597A] focus:border-[#20232E] focus:bg-[#15171E] focus:ring-2 focus:ring-[#B19DC5] text-base outline-none text-[#EDEDED] py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                                         placeholder="Insert your name" required
                                         id="name"
                                         type="name"
@@ -54,10 +54,10 @@ function ContactForm() {
                                         field="name"
                                         errors={state.errors}
                                     />
-                                    <label className="leading-7 text-sm text-[#D9D9D9]" htmlFor="email">
+                                    <label className="leading-7 text-sm text-[#1E1E1E]" htmlFor="email">
                                         Email
                                     </label>
-                                    <input className="w-full bg-[#1D202B] bg-opacity-40 rounded border border-[#4E597A] focus:border-[#20232E] focus:bg-[#15171E] focus:ring-2 focus:ring-[#B19DC5] text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                                    <input className="w-full bg-[#1D202B] rounded border border-[#4E597A] focus:border-[#20232E] focus:bg-[#15171E] focus:ring-2 focus:ring-[#B19DC5] text-base outline-none text-[#EDEDED] py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                                         placeholder="Insert your email" required
                                         id="email"
                                         type="email"
@@ -68,8 +68,8 @@ function ContactForm() {
                                         field="email"
                                         errors={state.errors}
                                     />
-                                    <label htmlFor="message" className="leading-7 text-sm text-[#D9D9D9]">Message</label>
-                                    <textarea className="w-full bg-[#1D202B] bg-opacity-40 rounded border border-[#4E597A] focus:border-[#20232E] focus:bg-[#15171E] focus:ring-2 focus:ring-[#B19DC5] h-32 text-base outline-none text-gray-100 py-1 px-3 leading-6 transition-colors duration-200 ease-in-out"
+                                    <label htmlFor="message" className="leading-7 text-sm text-[#1E1E1E]">Message</label>
+                                    <textarea className="w-full bg-[#1D202B] rounded border border-[#4E597A] focus:border-[#20232E] focus:bg-[#15171E] focus:ring-2 focus:ring-[#B19DC5] h-32 text-base outline-none text-[#EDEDED] py-1 px-3 leading-6 transition-colors duration-200 ease-in-out"
                                         placeholder="Insert your message" required
                                         id="message"
                                         name="message"
@@ -89,22 +89,21 @@ function ContactForm() {
                         </div>
                         <section id="footer">
                             <div className="w-full pt-8 mt-8 border-t border-gray-800 text-center">
-                                <a className="text-[#B19DC5] hover:text-[#D8C5EC] block leading-normal my-5 text-lg" href="mailto:najwatahir186@gmail.com">najwatahir186@email.com</a>
+                                <a className="text-[#1E1E1E] hover:text-[#70667a] block leading-normal my-5 text-lg" href="mailto:najwatahir186@gmail.com">najwatahir186@email.com</a>
                                 <span className="inline-flex pb-5">
-                                    <a className="ml-4 text-[#D9D9D9] hover:text-[#A59CB0]" href="https://www.instagram.com/najwathalibz/" target="_blank">
+                                    <a className="ml-4 text-[#1E1E1E] hover:text-[##70667a]" href="https://www.instagram.com/najwathalibz/" target="_blank">
                                         <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
                                             <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
                                             <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
                                         </svg>
                                     </a>
-                                    <a href="https://github.com/najwatahir" target="_blank" className="ml-4 text-[#D9D9D9] hover:text-[#A59CB0]">
+                                    <a href="https://github.com/najwatahir" target="_blank" className="ml-4 text-[#1E1E1E] hover:text-[##70667a]">
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="w-5 h-5" viewBox="0 0 16 16">
                                             <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
                                         </svg>
                                     </a>
                                 </span>
                             </div>
-                            <p className="text-semibold text-[#D9D9D9] text-center">Copyright &#169; 2023 Najwa Tahir. All right reserved.</p>
                         </section>
 
                     </div>

41a27cbf423fd63e7d6eba83d7c9debce10b5cbe
Najwa Tahir <110643223+najwatahir@users.noreply.github.com>
2023-08-03 12:52:25 +0800

    update social media

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -91,11 +91,8 @@ function ContactForm() {
                             <div className="w-full pt-8 mt-8 border-t border-gray-800 text-center">
                                 <a className="text-[#1E1E1E] hover:text-[#70667a] block leading-normal my-5 text-lg" href="mailto:najwatahir186@gmail.com">najwatahir186@email.com</a>
                                 <span className="inline-flex pb-5">
-                                    <a className="ml-4 text-[#1E1E1E] hover:text-[#70667a]" href="https://www.instagram.com/najwathalibz/" target="_blank">
-                                        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
-                                            <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
-                                            <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
-                                        </svg>
+                                    <a className="ml-4 text-[#1E1E1E] hover:text-[#70667a]" href="https://www.linkedin.com/in/najwatahir/" target="_blank">
+                                        <svg xmlns="http://www.w3.org/2000/svg" className='w-5 h-5' fill="currentColor" viewBox="0 0 16 16"> <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z" /> </svg>
                                     </a>
                                     <a href="https://github.com/najwatahir" target="_blank" className="ml-4 text-[#1E1E1E] hover:text-[#70667a]">
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="w-5 h-5" viewBox="0 0 16 16">

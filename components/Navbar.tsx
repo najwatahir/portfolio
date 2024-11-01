b7d5f1874ed331f29905538906138761e5768646
Najwa Tahir <najwatahir186@gmail.com>
2024-11-01 18:51:24 +0800

    revised bug

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,8 @@
 "use client"
 
-import React from "react"
-import { useState } from "react"
+import React, { useState } from "react"
+import Image from "next/image"
+
 import { Link } from "react-scroll/modules"
 import { IoMdMenu, IoMdClose } from "react-icons/io"
 
@@ -32,14 +33,19 @@ const NAV_ITEMS: Array<NavItem> = [
 export default function Navbar() {
     const [navbar, setNavbar] = useState(false)
     return (
-        <header className="w-full mx-auto px-4 sm:px-20 fixed top-0 z-50 bg-[#DAD7E3]">
-            <div className="justify-between md:items-center md:flex">
+        <header className="w-full mx-auto px-4 sm:px-20 fixed top-0 z-50 bg-[#ffff]">
+            <div className="justify-between md:items-center md:flex rubik">
                 <div>
                     <div className="flex items-center justify-between py-3 md:py-5 md:block">
                         <Link to="home">
                             <div className="container flex items-center space-x-2">
-                                <img src="/images/catmeiw.png" className="w-10" alt="" />
-                                <h2 className="text-2xl font-bold text-[#1B1F2C]">NAJWA</h2>
+                                <Image
+                                    src="/images/logo-nat.png"
+                                    alt=""
+                                    width={40}
+                                    height={40}
+                                    className=""
+                                />
                             </div>
                         </Link>
                         <div className="md:hidden">
--- components/Project.tsx
+++ components/Project.tsx
@@ -1,93 +1,108 @@
-"use client"
-
-import React, { useState } from "react"
+import React from "react"
 import Image from "next/image"
 
-import { Link } from "react-scroll/modules"
-import { IoMdMenu, IoMdClose } from "react-icons/io"
-
-interface NavItem {
-    label: string
-    page: string
-}
-
-const NAV_ITEMS: Array<NavItem> = [
-    {
-        label: "Home",
-        page: "home",
-    },
-    {
-        label: "About",
-        page: "about",
-    },
-    {
-        label: "Project",
-        page: "project",
-    },
-    {
-        label: "Contact",
-        page: "contact",
-    },
-]
-
-export default function Navbar() {
-    const [navbar, setNavbar] = useState(false)
+const Project = () => {
     return (
-        <header className="w-full mx-auto px-4 sm:px-20 fixed top-0 z-50 bg-[#ffff]">
-            <div className="justify-between md:items-center md:flex rubik">
-                <div>
-                    <div className="flex items-center justify-between py-3 md:py-5 md:block">
-                        <Link to="home">
-                            <div className="container flex items-center space-x-2">
-                                <Image
-                                    src="/images/logo-nat.png"
-                                    alt=""
-                                    width={40}
-                                    height={40}
-                                    className=""
-                                />
+        <section id="project" className="bg-[#f3eef8] pt-5 rubik">
+            <h1 className="my-5 text-center font-bold text-2xl md:text-4xl text-[#1E1E1E]">
+                PROJECT
+                <hr className="w-12 h-1 mx-auto my-4 bg-[#fcdf78] border-0 rounded"></hr>
+            </h1>
+            <div className="container px-5 py-5 mx-auto rubik">
+                <div className="flex flex-wrap justify-center">
+                    <div className="project-box sm:w-1/2 lg:w-1/4 rounded-lg h-1/4">
+                        <div className="h-fit rounded-lg overflow-hidden cursor-default">
+                            <Image
+                                src="/images/api-documentation.jpeg"
+                                alt="API Documentation"
+                                width={325}
+                                height={325}
+                                className="lg:h-40 w-screen object-cover object-center"
+                            />
+                            <div className="p-4">
+                                <h1 className="text-xl font-semibold mb-3">API Documentation</h1>
+                                <p className="leading-relaxed mb-3">A simple REST API Documentation made with ExpressJS and Prisma</p>
+                                <div className="flex items-center flex-wrap ">
+                                    <a href="https://documenter.getpostman.com/view/29807114/2s9YXk52Ug" target="_blank"><button>
+                                        <Image
+                                            src="/images/browser-icon.png"
+                                            alt=""
+                                            width={20}
+                                            height={20}
+                                            className=""
+                                        />
+                                    </button>
+                                    </a>
+                                    <a href="https://github.com/najwatahir/sumeru-akademiya-api" target="_blank"><button>
+                                        <Image
+                                            src="/images/github-icon.png"
+                                            alt=""
+                                            width={20}
+                                            height={20}
+                                            className="ml-2"
+                                        />
+                                    </button>
+                                    </a>
+                                </div>
                             </div>
-                        </Link>
-                        <div className="md:hidden">
-                            <button
-                                className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
-                                onClick={() => setNavbar(!navbar)}
-                            >
-                                {navbar ? <IoMdClose size={30} /> : <IoMdMenu size={30} />}
-                            </button>
                         </div>
                     </div>
-                </div>
-
-                <div>
-                    <div
-                        className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${navbar ? "block" : "hidden"
-                            }`}
-                    >
-                        <div className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
-                            {NAV_ITEMS.map((item, idx) => {
-                                return (
-                                    <Link
-                                        key={idx}
-                                        to={item.page}
-                                        className={
-                                            "block lg:inline-block text-[#1B1F2C] font-semibold hover:text-[#485375] md:pl-3 md:pr-3 cursor-pointer scroll-pt-5"
-                                        }
-                                        activeClass="active"
-                                        spy={true}
-                                        smooth={true}
-                                        offset={-79}
-                                        duration={100}
-                                        onClick={() => setNavbar(!navbar)}
-                                    >
-                                        {item.label}
-                                    </Link>
-                                )
-                            })}
+                    <div className="project-box sm:w-1/2 lg:w-1/4 rounded-lg h-1/4">
+                        <div className="h-fit rounded-lg overflow-hidden cursor-default">
+                            <Image
+                                src="/images/card-web.jpeg"
+                                alt="Personal Card Website"
+                                width={325}
+                                height={325}
+                                className="lg:h-40 w-screen object-cover object-center"
+                            />
+                            <div className="p-4">
+                                <h1 className="text-2xl font-semibold mb-3">Personal Card Website</h1>
+                                <p className="leading-relaxed mb-3">A website that contains some of my personal information. Made with NextJS & Tailwind</p>
+                                <div className="flex items-center flex-wrap ">
+                                    <a href="https://annetarc.vercel.app/" target="_blank"><button>
+                                        <Image
+                                            src="/images/browser-icon.png"
+                                            alt=""
+                                            width={20}
+                                            height={20}
+                                            className=""
+                                        />
+                                    </button>
+                                    </a>
+                                    <a href="https://github.com/najwatahir/annetarc-card" target="_blank"><button>
+                                        <Image
+                                            src="/images/github-icon.png"
+                                            alt=""
+                                            width={20}
+                                            height={20}
+                                            className="ml-2"
+                                        />
+                                    </button>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="project-box sm:w-1/2 lg:w-1/4 rounded-lg h-1/4">
+                        <div className="h-fit rounded-lg overflow-hidden cursor-default">
+                            <Image
+                                src="/images/literalfabet.jpeg"
+                                alt="Literalfabet"
+                                width={325}
+                                height={325}
+                                className="lg:h-40 w-screen object-cover object-center"
+                            />
+                            <div className="p-4">
+                                <h1 className="text-2xl font-semibold mb-3">Literalfabet</h1>
+                                <p className="leading-relaxed mb-3">A bookstore website made with PHP. This website is currently still under development.</p>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </header>
+        </section >
     )
 }
+
+export default Project
